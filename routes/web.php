4e956fad0db65dfaeae2720dc2aa54e4e2d60d4a
ajins15 <ansengarie@gmail.com>
2021-10-01 15:12:11 +0700

    structuring folder, routing and make views

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view(
+        'about',
+        [
+            "name" => "Aji Nuansa Sengarie",
+            "email" => "ansengarie@gmail.com",
+            "image" => "profile-pic.png"
+        ]
+    );
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
