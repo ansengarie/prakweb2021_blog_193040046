2a88a8c19ed8500a90c7832a7d19fd750f2a63aa
ajins15 <ansengarie@gmail.com>
2021-10-01 16:52:55 +0700

    blade templating engine

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,21 +1,8 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.main')
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
-    <title>WPU | About</title>
-  </head>
-  <body>
-    <h1>Halaman About</h1>
-    <img src="img/<?= $image; ?>" alt="<?= $name; ?>" class="rounded-circle mx-auto d-block" width="200">
-    <h3 class="text-center"><?= $name; ?></h3>
-    <p class="text-center"><?= $email; ?></p>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
-    
-  </body>
-</html>
+@section('container')
+<h1>Halaman About</h1>
+<img src="img/{{ $image }}" alt="{{ $name }}" class="rounded-circle mx-auto d-block" width="200">
+<h3 class="text-center">{{ $name }}</h3>
+<p class="text-center">{{ $email }}</p>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view(
         'about',
         [
+            "title" => "About",
             "name" => "Aji Nuansa Sengarie",
             "email" => "ansengarie@gmail.com",
             "image" => "profile-pic.png"
@@ -29,5 +32,53 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Aji Nuansa Sengarie",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi commodi expedita unde sit doloribus. Perspiciatis numquam architecto voluptates hic repellendus nulla quasi voluptatum quos, sunt, dolor doloribus inventore ea officiis aliquid dolorum impedit omnis cumque nobis, illo fugit modi. Rerum deserunt eaque eveniet vero, blanditiis aut necessitatibus a, deleniti accusantium sint aspernatur atque explicabo voluptas sequi iusto iure hic! Maiores quo, omnis architecto amet error dignissimos hic impedit laboriosam aliquam fugit ad blanditiis rem odio, quaerat doloribus ipsam tempore deserunt."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Fauzan Nur Salma",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti ea fuga aperiam natus dignissimos, quis expedita laudantium eligendi nisi odit, aspernatur necessitatibus porro, magnam dolorem quidem est consequuntur dolores vitae repellat quod. Deleniti earum amet facere quidem explicabo incidunt dolores dolor quibusdam suscipit architecto similique alias quas vitae iusto veniam quasi enim nisi iste harum unde, iure nostrum ex blanditiis? Repellendus et quae deleniti fugiat libero nulla modi qui esse?"
+        ]
+    ];
+
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman single post
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Aji Nuansa Sengarie",
+            "body" => "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi commodi expedita unde sit doloribus. Perspiciatis numquam architecto voluptates hic repellendus nulla quasi voluptatum quos, sunt, dolor doloribus inventore ea officiis aliquid dolorum impedit omnis cumque nobis, illo fugit modi. Rerum deserunt eaque eveniet vero, blanditiis aut necessitatibus a, deleniti accusantium sint aspernatur atque explicabo voluptas sequi iusto iure hic! Maiores quo, omnis architecto amet error dignissimos hic impedit laboriosam aliquam fugit ad blanditiis rem odio, quaerat doloribus ipsam tempore deserunt."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Fauzan Nur Salma",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corrupti ea fuga aperiam natus dignissimos, quis expedita laudantium eligendi nisi odit, aspernatur necessitatibus porro, magnam dolorem quidem est consequuntur dolores vitae repellat quod. Deleniti earum amet facere quidem explicabo incidunt dolores dolor quibusdam suscipit architecto similique alias quas vitae iusto veniam quasi enim nisi iste harum unde, iure nostrum ex blanditiis? Repellendus et quae deleniti fugiat libero nulla modi qui esse?"
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post['slug'] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
