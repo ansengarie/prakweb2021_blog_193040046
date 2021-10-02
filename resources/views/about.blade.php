82c112c92c6b5ece3bb9a0d0c223f52d6705386f
ajins15 <ansengarie@gmail.com>
2021-10-02 20:06:26 +0700

    create Post model and controller, and using collection

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,7 +2,7 @@
 
 @section('container')
 <h1>Halaman About</h1>
-<img src="img/{{ $image }}" alt="{{ $name }}" class="rounded-circle mx-auto d-block" width="200">
+<img src="img/{{ $image }}" alt="{{ $name }}" class="img-thumbnail rounded-circle mx-auto d-block" width="200">
 <h3 class="text-center">{{ $name }}</h3>
 <p class="text-center">{{ $email }}</p>
 @endsection
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,8 +3,7 @@
 @section('container')
 <article>
   <h2>{{ $post['title'] }}</h2>
-  <h5>{{ $post['author'] }}</h5>
-  <p>{{ $post['body'] }}</p>
+  <p class="mt-5">{{ $post['body'] }}</p>
 </article>
 
 <a href="/blog" class="btn btn-primary text-decoration-none">Back to Posts</a>
