443dd7efb1832b92b32261c635dbab37f3d34f02
ajins15 <ansengarie@gmail.com>
2021-10-07 23:45:31 +0700

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,63 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        User::create([
+            'name' => 'Aji Nuansa Sengarie',
+            'email' => 'ansengarie@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        User::create([
+            'name' => 'Fauzan Nur Salma',
+            'email' => 'fauzanns@gmail.com',
+            'password' => bcrypt('12345')
+        ]);
+
+        Category::create([
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
+        ]);
+
+        Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+
+        Post::create([
+            'title' => 'Judul Pertama',
+            'slug' => 'judul-pertama',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos praesentium, reprehenderit dolorum a magni doloremque fugiat vel assumenda animi, dicta consequuntur exercitationem delectus. Debitis molestiae aut magnam accusamus, quisquam, animi nostrum totam perspiciatis amet ullam itaque nisi vel assumenda, natus suscipit nesciunt autem excepturi non vitae quis. Reprehenderit corporis sint minima dolor similique hic officiis, voluptas dolores incidunt rem ipsam voluptates odit quod ut rerum vel, in laborum placeat! Quia, nihil! Hic corporis tempore esse sapiente debitis eum atque sequi aspernatur aperiam, odio consequatur rem saepe, magni soluta ad laborum. Id a atque qui obcaecati nihil adipisci consequatur. Nobis, accusantium?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Kedua',
+            'slug' => 'judul-kedua',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos praesentium, reprehenderit dolorum a magni doloremque fugiat vel assumenda animi, dicta consequuntur exercitationem delectus. Debitis molestiae aut magnam accusamus, quisquam, animi nostrum totam perspiciatis amet ullam itaque nisi vel assumenda, natus suscipit nesciunt autem excepturi non vitae quis. Reprehenderit corporis sint minima dolor similique hic officiis, voluptas dolores incidunt rem ipsam voluptates odit quod ut rerum vel, in laborum placeat! Quia, nihil! Hic corporis tempore esse sapiente debitis eum atque sequi aspernatur aperiam, odio consequatur rem saepe, magni soluta ad laborum. Id a atque qui obcaecati nihil adipisci consequatur. Nobis, accusantium?',
+            'category_id' => 1,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Ketiga',
+            'slug' => 'judul-ketiga',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos praesentium, reprehenderit dolorum a magni doloremque fugiat vel assumenda animi, dicta consequuntur exercitationem delectus. Debitis molestiae aut magnam accusamus, quisquam, animi nostrum totam perspiciatis amet ullam itaque nisi vel assumenda, natus suscipit nesciunt autem excepturi non vitae quis. Reprehenderit corporis sint minima dolor similique hic officiis, voluptas dolores incidunt rem ipsam voluptates odit quod ut rerum vel, in laborum placeat! Quia, nihil! Hic corporis tempore esse sapiente debitis eum atque sequi aspernatur aperiam, odio consequatur rem saepe, magni soluta ad laborum. Id a atque qui obcaecati nihil adipisci consequatur. Nobis, accusantium?',
+            'category_id' => 2,
+            'user_id' => 1
+        ]);
+
+        Post::create([
+            'title' => 'Judul Keempat',
+            'slug' => 'judul-keempat',
+            'excerpt' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos praesentium, reprehenderit dolorum a magni doloremque fugiat vel assumenda animi, dicta consequuntur exercitationem delectus. Debitis molestiae aut magnam accusamus, quisquam, animi nostrum totam perspiciatis amet ullam itaque nisi vel assumenda, natus suscipit nesciunt autem excepturi non vitae quis. Reprehenderit corporis sint minima dolor similique hic officiis, voluptas dolores incidunt rem ipsam voluptates odit quod ut rerum vel, in laborum placeat! Quia, nihil! Hic corporis tempore esse sapiente debitis eum atque sequi aspernatur aperiam, odio consequatur rem saepe, magni soluta ad laborum. Id a atque qui obcaecati nihil adipisci consequatur. Nobis, accusantium?',
+            'category_id' => 2,
+            'user_id' => 2
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,11 +4,11 @@
 <article>
   <h2>{{ $post->title }}</h2>
 
-  <p>By. Aji Nuansa Sengarie <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+  <p>By. <a href="#" class="text-decoration-none">{{ $post->user->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
  {!! $post->body !!}
 </article>
 
-<a href="/blog" class="btn btn-primary text-decoration-none">Back to Posts</a>
+<a href="/blog" class="btn btn-primary text-decoration-none mt-3">Back to Posts</a>
     
 @endsection
 
